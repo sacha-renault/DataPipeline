f62714cd656df38ba7cac089fefc92c6271bcd71
sacha-renault <sacha35340@gmail.com>
2024-09-15 14:25:30 +0200

    remove: template useless validator

--- src/dl_data_pipeline/validator/new_validator.py
+++
@@ -1,8 +0,0 @@
-from typing import Any
-from .base_validator import Validator
-from .validation_error import ValidationError
-
-class NameValidator(Validator):
-    def validate(self, data: Any) -> None:
-        pass
-        # TODO
