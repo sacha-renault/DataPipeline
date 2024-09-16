3eb45e28d834985893fc2f55b9b85c08e317ef3d
sacha-renault <sacha35340@gmail.com>
2024-09-16 09:23:13 +0200

    fix (test): add __init__.py on validator test folder

---
+++ test/validator/__init__.py
@@ -0,0 +1,10 @@
+class DummyData:
+    def __init__(self, shape):
+        self.shape = shape
+
+class CustomShapeData:
+    def __init__(self, custom_shape):
+        self.custom_shape_attr = custom_shape
+
+class CustomClass:
+        pass
