a3b67215edcae3e8327f5b16a47d3304fce44cc1
sacha-renault <sacha35340@gmail.com>
2024-09-17 15:25:20 +0200

    fix (deferred): remove duplicated code

--- src/dl_data_pipeline/pipeline/pipe_node.py
+++ src/dl_data_pipeline/pipeline/pipe_node.py
@@ -39,37 +39,3 @@ class PipeNode:
 
     def _set_value(self, value: Any) -> None:
         self.__value = value
-
-def deferred_execution(func):
-    """
-    A decorator that defers the execution of a function until the actual data is provided.
-
-    The first argument of the function must be either DATA_PLACEHOLDER or None, indicating
-    that the function execution should be deferred. When the function is eventually called
-    with the actual data, the deferred execution is triggered.
-
-    Args:
-        func (callable): The function to be deferred.
-
-    Returns:
-        callable: A lambda function that takes the data as its first argument and executes
-                  the original function with the deferred arguments.
-    """
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        # Split args into Node / Non Node
-        args_no_data = []
-        parent = []
-        for arg in args:
-            if isinstance(arg, PipeNode):
-                parent.append(arg)
-            else:
-                args_no_data.append(arg)
-
-        # kw_no_data = {k: v for k, v in kwargs.items() if not isinstance(v, PipeNode)}
-        deferred_func = lambda *data: func(*data, *args_no_data, **kwargs)
-        deferred_func.__name__ = func.__name__
-        return PipeNode(deferred_func, parent)
-    return wrapper
-
-
